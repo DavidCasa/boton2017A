32f48699d1a2fa6720eb2400941c6d416386e2f8
Fabricio\Jhon <maria.cordova01@epn.edu.ec>
2017-05-20 12:05:40 -0500

    version2

--- boton2017A.py
+++ boton2017A.py
@@ -1,9 +1,15 @@
 def fun1():
     print('David Casa')
 
+def fun2():
+    print('Fernanda Cordova')
+
+
 from tkinter import *
 ventana = Tk()
 ventana.title('Deber Botones')
 boton1=Button(ventana,text="David Casa",command=fun1, bg='red')
 boton1.grid(row=1,column=1)
+boton2=Botton(ventana,text="Fernanda Cordova",command=fun2)
+boton2.grid(row=1,column=2)
 
