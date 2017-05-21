cdad28b1089ed87d6a32e67637bc355ffd0f1658
Geovanny Dias <geova_jd@hotmail.com>
2017-05-21 14:32:40 -0500

    Verificacion y consolidacion de codigo final

--- boton2017A.py
+++ boton2017A.py
@@ -1,3 +1,5 @@
+from tkinter import *
+
 def fun1():
     print('David Casa')
 
@@ -7,14 +9,46 @@ def fun2():
 def fun3():
     print('Ivonne Vega')
 
+def fun4():
+    print('Geovanny Dias')
 
-from tkinter import *
+def botones():
+    
+    btn1 = Button(ventana, text="Nombre Estudiante 1", command = fun1)
+    btn1.pack()
+
+    btn2 = Button(ventana, text="Nombre Estudiante 2", command = fun2)
+    btn2.pack()
+
+    btn3 = Button(ventana, text="Nombre Estudiante 3", command = fun3)
+    btn3.pack()
+
+    btn4 = Button(ventana, text="Nombre Estudiante 4", command = fun4)
+    btn4.pack()
+
+    btn.config(state=DISABLED)
+
 ventana = Tk()
 ventana.title('Deber Botones')
-boton1=Button(ventana,text="David Casa",command=fun1, bg='red')
+btn = Button(ventana, text = "Click Nombre Estudintes", command = botones)
+btn.pack()
+
+
+
+
+
+"""
+boton1=Button(ventana,text="David Casa",command=fun1)
 boton1.grid(row=1,column=1)
-boton2=Botton(ventana,text="Fernanda Cordova",command=fun2)
+boton2=Button(ventana,text="Fernanda Cordova",command=fun2)
 boton2.grid(row=1,column=2)
 boton3=Button(ventana,text="Ivonne Vega",command=fun3)
-boton3.grid(row=1,column=3)
+boton3.grid(row=2,column=1)
+boton4=Button(ventana,text="Nombre4",command=fun4)
+boton4.grid(row=2,column=2)
+"""
+
+
+
+
 
