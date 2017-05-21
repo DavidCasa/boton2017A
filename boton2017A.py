a2bbaf230c3e3412064b149f33253c3df1e6c1da
Ivonne Vega\PERSONAL <ivonne01101@hotmail.com>
2017-05-21 09:14:07 -0500

    Botones

--- boton2017A.py
+++ boton2017A.py
@@ -4,6 +4,9 @@ def fun1():
 def fun2():
     print('Fernanda Cordova')
 
+def fun3():
+    print('Ivonne Vega')
+
 
 from tkinter import *
 ventana = Tk()
@@ -12,4 +15,6 @@ boton1=Button(ventana,text="David Casa",command=fun1, bg='red')
 boton1.grid(row=1,column=1)
 boton2=Botton(ventana,text="Fernanda Cordova",command=fun2)
 boton2.grid(row=1,column=2)
+boton3=Button(ventana,text="Ivonne Vega",command=fun3)
+boton3.grid(row=1,column=3)
 
